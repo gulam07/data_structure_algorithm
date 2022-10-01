4f950f75b565d44d7bb336ad3f62107374378815
gulam07 <mohdghaus98@gmail.com>
2022-10-01 20:24:23 +0530

    dsa

---
+++ Recursion/factorial.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    int a=n*fact(n-1);
+    return a;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int ans=fact(n);
+    cout<<ans<<endl;
+    return 0;
+}
