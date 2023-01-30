25e0316abbe4771006b355b370d552e03dc5bf34
gulam07 <mohdghaus98@gmail.com>
2023-01-30 20:06:03 +0530

    dsa

---
+++ function_sakib/void_f1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool sakib(){
+    //cout<<"gulam";
+    return c;
+}
+
+int main(){
+    cout<<"Sakib" << endl;
+    string a=sakib();
+    cout<<a;
+    return 0;
+}
